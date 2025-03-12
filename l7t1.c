cd718f3127c545acecb5e874d174a5a6dfd43422
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-12 14:43:11 -0400

    first commit

---
+++ l7t1.c
@@ -0,0 +1,34 @@
+// This program multiplies 2 5x5 randomly-generated matrices
+// 6 different ways to compare if results would be identical.
+
+// include statements
+#include <stdio.h>
+
+// define statements
+#define SIZE 5
+
+// function declarations
+void createMatrix(int matrix[SIZE][SIZE]);
+void printMatrix(int matrix[SIZE][SIZE]);
+int compareMatrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE]);
+
+// main function
+int main() {
+    // TO DO
+}
+
+// This function populates a given matrix with random integers
+void createMatrix(int matrix[SIZE][SIZE]) {
+    // TODO
+}
+
+// This function prints a given matrix to the screen
+void printMatrix(int matrix[SIZE][SIZE]) {
+    // TODO
+}
+
+// This function compares the 2 matrices.
+// Returns 1 if equal, returns 0 if unequal
+int compareMatrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE]) {
+    // TODO
+}
