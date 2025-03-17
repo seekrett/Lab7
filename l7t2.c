f179b34d5ee01b9db110aff0354a807136a589d7
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-17 18:50:50 -0400

    added main body

--- l7t2.c
+++ l7t2.c
@@ -13,11 +13,57 @@ void multiplyMatrices(int size, double A[size][size], double B[size][size], doub
 
 // main function
 int main() {
-    // variable declarations
+    // universal variable declarations
     int sizes[] = {20, 40, 80, 160, 320, 640, 1280};        // matrix sizes
     int numOfSizes = sizeof(sizes) / sizeof(sizes[0]);      // total num of sizes 
     int numOfOrders = 6;                                    // num of multiplication orders
     
+    // for each size,
+    for (int i = 0; i < numOfSizes; i++) {
+        // local variable declarations
+        int size = sizes[i];                                // tracks current size dealing with
+        double A[size][size], B[size][size], C[size][size]; // initialize each matrix
+
+        // create a matrix
+        createMatrix(size, A);
+        createMatrix(size, B);
+
+        // print size
+        printf("\n----- MATRICES SIZE %dx%d -----\n", size, size);
+
+        // get product 6 different ways
+        for (int order = 0; order < numOfOrders; order++) {
+            // get time taken and multiply
+            clock_t start = clock();
+            multiplyMatrices(size, A, B, C, order);
+            clock_t end = clock();
+            double timeTaken = (double)(end - start) / CLOCKS_PER_SEC;
+
+            // print time taken
+            printf("\nTime taken: %.6f seconds\n", timeTaken);
+            // print order
+            switch (order) {
+                case 0:
+                printf("Order: ijk\n");
+                break;
+                case 1:
+                printf("Order: ikj\n");
+                break;
+                case 2:
+                printf("Order: kij\n");
+                break;
+                case 3:
+                printf("Order: jik\n");
+                break;
+                case 4:
+                printf("Order: jki\n");
+                break;
+                case 5:
+                printf("Order: kji\n");
+                break;
+            }
+        }
+    }
 }
 
 // This function populates random elements in a matrix of a given size.
