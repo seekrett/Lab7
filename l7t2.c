a4dec88349dd260ee23a98300d4cbded29255dd0
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-17 15:57:29 -0400

    created createMatrix(), random size

--- l7t2.c
+++ l7t2.c
@@ -18,7 +18,11 @@ int main() {
 
 // This function populates random elements in a matrix of a given size.
 void createMatrix(int size, double matrix[size][size]) {
-    // TODO
+    for (int i = 0; i < size; i ++) {
+        for (int j = 0; j < size; j++) {
+            matrix[i][j] = (double) rand() / RAND_MAX;
+        }
+    }
 }
 
 // This function multiplies two matrices of the same size in differing orders.
