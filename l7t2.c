709e5a854dfec2e385385f0b6449ad518abe6cb6
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-17 15:52:50 -0400

    created outline

---
+++ l7t2.c
@@ -0,0 +1,27 @@
+// This program compares the time taken to multiply matrices of different sizes
+// to check which process is more efficient in accessing memory.
+
+// include statements
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+// function declarations
+void createMatrix(int size, double matrix[size][size]);
+void multiplyMatrices(int size, double A[size][size], double B[size][size], double C[size][size], int order);
+
+// main function
+int main() {
+    //TODO
+}
+
+// This function populates random elements in a matrix of a given size.
+void createMatrix(int size, double matrix[size][size]) {
+    // TODO
+}
+
+// This function multiplies two matrices of the same size in differing orders.
+void multiplyMatrices(int size, double A[size][size], double B[size][size], double C[size][size], int order) {
+    //TODO
+}
