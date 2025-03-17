dd3e9059dcd943bd4b92e878cd21a0d0097e53dd
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-17 16:10:56 -0400

    created multiplyMatrices()

--- l7t2.c
+++ l7t2.c
@@ -27,5 +27,79 @@ void createMatrix(int size, double matrix[size][size]) {
 
 // This function multiplies two matrices of the same size in differing orders.
 void multiplyMatrices(int size, double A[size][size], double B[size][size], double C[size][size], int order) {
-    //TODO
+    // set product matrix C to 0
+    for (int i = 0; i < size; i ++) {
+        for (int j = 0; j < size; j++) {
+            C[i][j] = 0.0;
+        }
+    }
+
+    // multiply based on order
+    switch (order) {
+        // ijk
+        case 0:
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    C[i][j] = A[i][k] * B[k][j];
+                }
+            }
+        }
+        break;
+
+        // ikj
+        case 1:
+        for (int i = 0; i < size; i++) {
+            for (int k = 0; k < size; k++) {
+                for (int j = 0; j < size; j++) {
+                    C[i][j] = A[i][k] * B[k][j];
+                }
+            }
+        }
+        break;
+
+        // kij
+        case 2:
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        break;
+
+        // jik
+        case 3:
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size; i++) {
+                for (int k = 0; k < size; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        break;
+
+        // jki
+        case 4:
+        for (int j = 0; j < size; j++) {
+            for (int k = 0; k < size; k++) {
+                for (int i = 0; i < size; i++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        break;
+
+        // kji
+        case 5:
+        for (int k = 0; k < size; k++) {
+            for (int j = 0; j < size; j++) {
+                for (int i = 0; i < size; i++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        break;
+    }
 }
